ddac5a865b26ef4a78d4ce9fcc8fd92a49b2d1d1
minhhn2910 <minhhn2910@gmail.com>
2023-10-26 15:52:18 +0800

    add sample execution 1thread with stack and uint256

---
+++ include/cuevm_test.h
@@ -0,0 +1,9 @@
+#ifndef CUEVM_TEST_H
+#define CUEVM_TEST_H
+#include "stack.cuh"
+
+
+void test_arithmetic_operations();
+void test_stack();
+
+#endif // CUEVM_TEST_H
---
+++ include/opcode.h
@@ -0,0 +1,11 @@
+#ifndef OPCODE_H
+#define OPCODE_H
+
+#define ADD 0x01
+#define MUL 0x02
+
+#define POP 0x50
+#define PUSH1 0x60
+// Add other opcode definitions here as your VM expands
+
+#endif // OPCODE_H
