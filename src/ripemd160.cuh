2252e55fa95aa4d598c3b657c035f80421a7ec3a
chen <chencassc@gmail.com>
2024-03-14 16:28:42 +0800

    rename

--- src/ripemd160.cuh
+++ src/ripemd160.cuh
@@ -1,7 +1,7 @@
 #pragma once
 #include "include/utils.h"
 
-#define ROL(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
+#define ROTL32(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
 
 #define F(x, y, z) ((x) ^ (y) ^ (z))
 #define G(x, y, z) (((x) & (y)) | (~(x) & (z)))
@@ -12,62 +12,62 @@
 #define FF(a, b, c, d, e, x, s)        \
     {                                  \
         (a) += F((b), (c), (d)) + (x); \
-        (a) = ROL((a), (s)) + (e);     \
-        (c) = ROL((c), 10);            \
+        (a) = ROTL32((a), (s)) + (e);     \
+        (c) = ROTL32((c), 10);            \
     }
 #define GG(a, b, c, d, e, x, s)                       \
     {                                                 \
         (a) += G((b), (c), (d)) + (x) + 0x5a827999UL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 #define HH(a, b, c, d, e, x, s)                       \
     {                                                 \
         (a) += H((b), (c), (d)) + (x) + 0x6ed9eba1UL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 #define II(a, b, c, d, e, x, s)                        \
     {                                                  \
         (a) += IQ((b), (c), (d)) + (x) + 0x8f1bbcdcUL; \
-        (a) = ROL((a), (s)) + (e);                     \
-        (c) = ROL((c), 10);                            \
+        (a) = ROTL32((a), (s)) + (e);                     \
+        (c) = ROTL32((c), 10);                            \
     }
 #define JJ(a, b, c, d, e, x, s)                       \
     {                                                 \
         (a) += J((b), (c), (d)) + (x) + 0xa953fd4eUL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 #define FFF(a, b, c, d, e, x, s)       \
     {                                  \
         (a) += F((b), (c), (d)) + (x); \
-        (a) = ROL((a), (s)) + (e);     \
-        (c) = ROL((c), 10);            \
+        (a) = ROTL32((a), (s)) + (e);     \
+        (c) = ROTL32((c), 10);            \
     }
 #define GGG(a, b, c, d, e, x, s)                      \
     {                                                 \
         (a) += G((b), (c), (d)) + (x) + 0x7a6d76e9UL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 #define HHH(a, b, c, d, e, x, s)                      \
     {                                                 \
         (a) += H((b), (c), (d)) + (x) + 0x6d703ef3UL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 #define III(a, b, c, d, e, x, s)                       \
     {                                                  \
         (a) += IQ((b), (c), (d)) + (x) + 0x5c4dd124UL; \
-        (a) = ROL((a), (s)) + (e);                     \
-        (c) = ROL((c), 10);                            \
+        (a) = ROTL32((a), (s)) + (e);                     \
+        (c) = ROTL32((c), 10);                            \
     }
 #define JJJ(a, b, c, d, e, x, s)                      \
     {                                                 \
         (a) += J((b), (c), (d)) + (x) + 0x50a28be6UL; \
-        (a) = ROL((a), (s)) + (e);                    \
-        (c) = ROL((c), 10);                           \
+        (a) = ROTL32((a), (s)) + (e);                    \
+        (c) = ROTL32((c), 10);                           \
     }
 
 __host__ __device__ static void compress(uint32_t* MDbuf, uint32_t* X)
